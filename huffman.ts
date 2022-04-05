cbcfb20c1a708056df0ab2c54fa2a584f64697c4
Pix3lAssassin <th3koolperson@gmail.com>
2022-04-04 23:24:01 -0600

    completed huffman encoding

--- huffman.ts
+++ huffman.ts
@@ -72,7 +72,7 @@ class BinaryTree<T> {
     currentNode.dfs(
       callback,
       currentNode.right,
-      position < 0 ? 1 : (position >>> 0) << (1 + 1),
+      position < 0 ? 1 : ((position >>> 0) << 1) + 1,
       depth + 1
     );
   }
@@ -96,22 +96,21 @@ class BinaryTree<T> {
 
   toArray(defaultValue: T, modifier: (value: T) => T = (value) => value): T[] {
     const result = [];
-    const queue: BinaryTree<T>[] = [this];
-
-    while (queue.length > 0) {
-      const node = queue.shift();
-
-      result.push(modifier(node.value));
-      if (node.left) {
-        queue.push(node.left);
-      } else {
-        queue.push(new BinaryTree(defaultValue))
+    const recurse = (currentNode: BinaryTree<T> = this, position = 0) => {
+      if (currentNode === null) {
+        return;
       }
 
-      if (node.right) {
-        queue.push(node.right);
-      } else {
-        queue.push(new BinaryTree(defaultValue))
+      result[position] = modifier(currentNode.value);
+      recurse(currentNode.left, position * 2 + 1);
+      recurse(currentNode.right, position * 2 + 2);
+    };
+
+    recurse();
+
+    for (let i = 0; i < result.length; i++) {
+      if (result[i] == null) {
+        result[i] = defaultValue;
       }
     }
 
@@ -120,7 +119,6 @@ class BinaryTree<T> {
 }
 
 export const encodeHuffman = (str): Blob => {
-  const buffer: number[] = [];
   const codeArr = [];
   const count: { [key: string]: number } = {};
   const queue = new PQueue<BinaryTree<number>>();
@@ -165,9 +163,36 @@ export const encodeHuffman = (str): Blob => {
     }
   });
 
-  treeData.bfs((value) => {
-    buffer.push(value ? value & 255 : 0);
+  const treeArr = treeData.toArray(27, (value) => {
+    return value ? value & 255 : 0;
   });
+
+  let oldValue: number = null;
+  let run = 1;
+  const buffer = treeArr.reduce((memo: number[], value, index) => {
+    if (value !== oldValue) {
+      if (run > 3) {
+        memo.push(26, run - 1);
+      } else if (run > 1) {
+        memo.push(...Array(run - 1).fill(oldValue));
+      }
+
+      memo.push(value);
+      oldValue = value;
+      run = 1;
+    } else {
+      run++;
+      if (index === treeArr.length - 1) {
+        if (run > 3) {
+          memo.push(26, run - 1);
+        } else if (run > 1) {
+          memo.push(...Array(run - 1).fill(oldValue));
+        }
+      }
+    }
+
+    return memo;
+  }, []);
 
   buffer.push(2);
 
@@ -176,10 +201,10 @@ export const encodeHuffman = (str): Blob => {
     bitCount += map[value][1];
   });
 
-  let position = 8 - (bitCount % 8);
+  const position = bitCount % 8;
 
-  let byte = position < 8 ? 1 << position : 0;
-  let offset = position;
+  let byte = position === 0 ? 0 : 1 << position;
+  let offset = position === 0 ? 8 : position;
   let leftOverBits = 0;
   const mask = [1, 3, 7, 15, 31, 63, 127, 255];
   codeArr.forEach((value) => {
@@ -187,15 +212,18 @@ export const encodeHuffman = (str): Blob => {
     offset -= map[value][1];
 
     if (offset <= 0) {
-      leftOverBits = Math.abs(offset)
+      leftOverBits = Math.abs(offset);
       byte += mappedCode >>> leftOverBits;
       buffer.push(byte);
+      byte = 0;
       offset = 8;
     }
 
+    // TODO if leftOverBits is greater than 8 meaning character data has to span over > 2 bytes
+
     if (leftOverBits > 0) {
       offset -= leftOverBits;
-      const maskedBits = mappedCode & mask[leftOverBits];
+      const maskedBits = mappedCode & mask[leftOverBits - 1];
       leftOverBits = 0;
     }
 
@@ -209,12 +237,11 @@ export const encodeHuffman = (str): Blob => {
 
 export const decodeHuffman = (str) => {};
 
-const str = "CBBDCCDBAEAEBEBBEEBBCCDACECADDBEBADABEBDBEBBCDCABDCBDEAACBDCACBDDCDEDEEBAEEBAEACCBBACBABAABBBBBBBBBB";
+const str =
+  "This is a test!";
 const encodedData = encodeHuffman(str);
 encodedData.arrayBuffer().then((buffer) => {
-  console.log('Buffer: ', buffer);
-
-
+  console.log("Buffer: ", buffer);
 
   const bytes = new Uint8Array(buffer);
   const binaryStrArr: string[] = [];
@@ -223,13 +250,13 @@ encodedData.arrayBuffer().then((buffer) => {
 
     const zeroPadded = 8 - str.length;
     for (let i = 0; i < zeroPadded; i++) {
-      str = '0' + str;
+      str = "0" + str;
     }
 
     binaryStrArr.push(str);
   });
-  console.log('Binary String: ', binaryStrArr.join(' '));
+  console.log("Binary String: ", binaryStrArr.join(" "));
 });
-console.log("Initial String: ", str);
+console.log(`Initial String: ${str}, ${str.length}`);
 console.log("Encoded Data: ", encodedData);
 console.log("Decoded String: ", decodeHuffman(encodedData));
